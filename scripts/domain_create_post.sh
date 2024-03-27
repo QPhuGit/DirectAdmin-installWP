c0e5afffa139a07a8df2be50b78eca48386722a9
QPhuGit <118596567+QPhuGit@users.noreply.github.com>
2024-03-27 10:59:29 +0700

    Update domain_create_post.sh

--- scripts/domain_create_post.sh
+++ scripts/domain_create_post.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # CHECK CUSTOM PKG ITEM INSTALLWP
 if [[ $installWP != 'ON' ]]; then
-  exit 0;
-else
+  exit 0
+fi
+
 # Set the path to the user.conf file
 user_conf_file="/usr/local/directadmin/data/users/${username}/user.conf"
 
@@ -13,60 +14,58 @@ sed -i 's/installWP=ON/installWP=OFF/g' "$user_conf_file"
 # exec 2>/usr/local/directadmin/customscripts.error.log
 
 # SET UP DATABASE VARIABLES
-dbpass=$(openssl rand -base64 12) > /dev/null
-ext=$(openssl rand -hex 2) > /dev/null
+dbpass=$(openssl rand -base64 12)
+ext=$(openssl rand -hex 2)
 dbuser="wp${ext}"    # do not include the username_ for dataskq here as DA adds this
 wpconfigdbuser="${username}_wp${ext}"
-wpadminpass=$(openssl rand -base64 14) > /dev/null
+wpadminpass=$(openssl rand -base64 14)
+
 # CHECK IF WORDPRESS EXISTS
-if [ -f /home/$username/domains/$domain/public_html/$subdomain/index.php ]; then
-echo "WARNING: There appears to be an index file already located in this directory, which indicates that an installation is already present! Empty the directory before running the script again."
-exit
+if [ -f /home/$username/domains/$domain/public_html/index.php ]; then
+  echo "WARNING: There appears to be an index file already located in this directory, which indicates that an installation is already present! Empty the directory before running the script again."
+  exit
 else
-# DISABLE DIRECTADMIN INDEX.HTML FILE
-if [ -f /home/$username/domains/$domain/public_html/$subdomain/index.html ]; then
-mv /home/$username/domains/$domain/public_html/$subdomain/index.html{,.bak}
-fi
-# CREATE DATABASE
-/usr/bin/mysqladmin -uda_admin -p$(cat /usr/local/directadmin/conf/mysql.conf | grep pass | cut -d\= -f2 ) create ${wpconfigdbuser};
-echo "CREATE USER ${wpconfigdbuser}@'localhost' IDENTIFIED BY '${dbpass}';" | mysql -uda_admin -p$(cat /usr/local/directadmin/conf/mysql.conf | grep pass | cut -d\= -f2 );
-echo "GRANT ALL PRIVILEGES ON ${wpconfigdbuser}.* TO ${wpconfigdbuser} IDENTIFIED BY '${dbpass}';" | mysql -uda_admin -p$(cat /usr/local/directadmin/conf/mysql.conf | grep pass | cut -d\= -f2);
+  # DISABLE DIRECTADMIN INDEX.HTML FILE
+  if [ -f /home/$username/domains/$domain/public_html/index.html ]; then
+    mv /home/$username/domains/$domain/public_html/index.html{,.bak}
+  fi
 
-# DOWNLOAD WORDPRESS
-cd /home/$username/domains/$domain/public_html/
-su -s /bin/bash -c "/usr/local/bin/wp core download" $username
+  # CREATE DATABASE
+  /usr/bin/mysqladmin -uda_admin -p$(cat /usr/local/directadmin/conf/mysql.conf | grep pass | cut -d\= -f2 ) create ${wpconfigdbuser};
+  echo "CREATE USER ${wpconfigdbuser}@'localhost' IDENTIFIED BY '${dbpass}';" | mysql -uda_admin -p$(cat /usr/local/directadmin/conf/mysql.conf | grep pass | cut -d\= -f2 );
+  echo "GRANT ALL PRIVILEGES ON ${wpconfigdbuser}.* TO ${wpconfigdbuser} IDENTIFIED BY '${dbpass}';" | mysql -uda_admin -p$(cat /usr/local/directadmin/conf/mysql.conf | grep pass | cut -d\= -f2);
 
-# SET DATABASE DETAILS IN THE CONFIG FILE
-su -s /bin/bash -c "/usr/local/bin/wp config create --dbname=$wpconfigdbuser --dbuser=$wpconfigdbuser --dbpass=$dbpass --dbhost=localhost" $username
+  # DOWNLOAD WORDPRESS
+  cd /home/$username/domains/$domain/public_html/
+  su -s /bin/bash -c "/usr/local/bin/wp core download" $username
 
-# INSTALL WORDPRESS
-if [[ $ssl == 'ON' ]]; then
-su -s /bin/bash -c "/usr/local/bin/wp core install --url=https://$domain/ --admin_user=$username --admin_password=$wpadminpass --title="$domain" --admin_email=$username@$domain " $username
-su -s /bin/bash -c "/usr/local/bin/wp rewrite structure '/%postname%/'" $username
-printf "\n\nWORDPRESS LOGIN CREDENTIALS:\nURL: https://$domain/wp-admin/\nUSERNAME: $username\nPASSWORD: $wpadminpass\n\n"
-if [[ ! -h /home/$username/domains/$domain/private_html ]]; then
-echo "Making a symlink for https..."
-cd /home/$username/domains/$domain/
-rm -rf private_html
-su -s /bin/bash -c "ln -s public_html private_html" $username
-fi
-else
-su -s /bin/bash -c "/usr/local/bin/wp core install --url=http://$domain/ --admin_user=$username --admin_password=$wpadminpass --title="$domain" --admin_email=$username@$domain " $username
-su -s /bin/bash -c "/usr/local/bin/wp rewrite structure '/%postname%/'" $username
-printf "\n\nWORDPRESS LOGIN CREDENTIALS:\nURL: http://$domain/wp-admin/\nUSERNAME: $username\nPASSWORD: $wpadminpass\n\n"
-fi
+  # SET DATABASE DETAILS IN THE CONFIG FILE
+  su -s /bin/bash -c "/usr/local/bin/wp config create --dbname=$wpconfigdbuser --dbuser=$wpconfigdbuser --dbpass=$dbpass --dbhost=localhost" $username
 
-# ADD LOGIN DETAILS TO TEXT FILE
-printf "\n\nWORDPRESS LOGIN CREDENTIALS:\nURL: http://$domain/wp-admin/\nUSERNAME: $username\nPASSWORD: $wpadminpass\n\n" >> /home/$username/domains/$domain/public_html/.wp-details.txt
-chown $username. /home/$username/domains/$domain/public_html/.wp-details.txt
+  # INSTALL WORDPRESS
+  if [[ $ssl == 'ON' ]]; then
+    su -s /bin/bash -c "/usr/local/bin/wp core install --url=https://$domain/ --admin_user=$username --admin_password=$wpadminpass --title="$domain" --admin_email=$username@$domain " $username
+    su -s /bin/bash -c "/usr/local/bin/wp rewrite structure '/%postname%/'" $username
+    if [[ ! -h /home/$username/domains/$domain/private_html ]]; then
+      echo "Making a symlink for https..."
+      cd /home/$username/domains/$domain/
+      rm -rf private_html
+      su -s /bin/bash -c "ln -s public_html private_html" $username
+    fi
+  else
+    su -s /bin/bash -c "/usr/local/bin/wp core install --url=http://$domain/ --admin_user=$username --admin_password=$wpadminpass --title="$domain" --admin_email=$username@$domain " $username
+    su -s /bin/bash -c "/usr/local/bin/wp rewrite structure '/%postname%/'" $username
+  fi
+
+  printf "\n\nWORDPRESS LOGIN CREDENTIALS:\nURL: http://$domain/wp-admin/\nUSERNAME: $username\nPASSWORD: $wpadminpass\n\n"
+
+  # ADD LOGIN DETAILS TO TEXT FILE
+  printf "\n\nWORDPRESS LOGIN CREDENTIALS:\nURL: http://$domain/wp-admin/\nUSERNAME: $username\nPASSWORD: $wpadminpass\n\n" >> /home/$username/domains/$domain/public_html/.wp-details.txt
+  chown $username. /home/$username/domains/$domain/public_html/.wp-details.txt
 fi
 
 # DELETE DOLLY PLUGIN AND INSTALL LITESPEED CACHE
 su -s /bin/bash -c "/usr/local/bin/wp plugin delete hello" $username
-#su -s /bin/bash -c "/usr/local/bin/wp plugin install litespeed-cache classic-editor --activate" $username
-#su -s /bin/bash -c "/usr/local/bin/wp theme install blogus --activate" $username
-#su -s /bin/bash -c "/usr/local/bin/wp post create --post_type=post --post_title='Thanks for Install - PuaruVN'" $username
-#su -s /bin/bash -c "/usr/local/bin/wp plugin install litespeed-cache jetpack --activate" $username
 
 # CREATE .HTACCESS
 cat << EOF > /home/$username/domains/$domain/public_html/.htaccess
@@ -90,6 +89,3 @@ find . -type f -exec chmod 0644 {} \;
 # WORDPRESS SECURITY AND HARDENING
 chmod 400 /home/$username/domains/$domain/public_html/.wp-details.txt
 chmod 400 /home/$username/domains/$domain/public_html/wp-config.php
-
-fi
-exit 0;
